6ca80964ad6eeadfc1714fcf1d9ef042b2362bbe
udulaindunil <udulaindunil@gmail.com>
2019-09-10 03:47:23 +0530

    data hard coded

--- Vinora/src/main/resources/static/Vinora/src/app/retailer/registered-item/registered-item.component.ts
+++ Vinora/src/main/resources/static/Vinora/src/app/retailer/registered-item/registered-item.component.ts
@@ -8,9 +8,10 @@ import { Item } from 'src/app/item/item.model';
 })
 export class RegisteredItemComponent implements OnInit {
 
-  items: Item[] = [ new Item(1,'first Item','first Brand','The discription here','http://2.bp.blogspot.com/-1-LqHgUvJN4/Vm122nf8VRI/AAAAAAAAAEA/FyO4TcIr36M/s1600/masoor%2Bdal%2Bbenefits.png','https://www.unilever.com/Images/heartbrand_tcm244-408761.gif',1000,40.00,'new'),
-  new Item(1,'first Item','first Brand','The discription here','http://2.bp.blogspot.com/-1-LqHgUvJN4/Vm122nf8VRI/AAAAAAAAAEA/FyO4TcIr36M/s1600/masoor%2Bdal%2Bbenefits.png','https://www.unilever.com/Images/heartbrand_tcm244-408761.gif',1000,40.00,'new')]
-
+  items: Item[] = [ new Item(1,'first Item','first Brand','The discription here The discription here The discription here','https://5.imimg.com/data5/MF/XV/MY-17000375/masoor-malka-500x500.jpg','https://www.unilever.com/Images/heartbrand_tcm244-408761.gif',1000,40.00,'new'),
+  new Item(2,'Second Item','second Brand','The discription here The discription here','https://5.imimg.com/data5/KA/GF/MY-17000375/masoor-sabut-500x500.jpg','https://www.unilever.com/Images/heartbrand_tcm244-408761.gif',1000,40.00,'new'),
+  new Item(3,'Third Item','third Brand','The discription here The discription here','http://shoorinternational.com/wp-content/uploads/2015/05/red-masoor-dal.jpg','https://www.unilever.com/Images/heartbrand_tcm244-408761.gif',1000,40.00,'new')]
+  
   constructor() { }
 
   ngOnInit() {
