4cfa6de427364b6baef0d190663e0a249001708c
udulaindunil <udulaindunil@gmail.com>
2019-09-15 10:09:18 +0530

    serv

--- Vinora/src/main/resources/static/Vinora/src/app/manager/register-new-item/register-new-item.component.ts
+++ Vinora/src/main/resources/static/Vinora/src/app/manager/register-new-item/register-new-item.component.ts
@@ -11,36 +11,37 @@ import { Router } from '@angular/router';
 })
 export class RegisterNewItemComponent implements OnInit {
 
-
+  selectedItemImage:File = null;
 
   constructor(private itemService: ItemServiceService,private route: Router) {
    }
+
+   onFileSelected(event){
+    this.selectedItemImage= <File>event.target.files[0];
+    
+  }
+   
 
   ngOnInit() {
     
   }
 
   onAddItem(form: NgForm){
-    // const value = form.value;
-    // const item = new Item();
-    // item.newItem(1,value.itemName,value.brandName,'',value.itemImage,value.brandImage,value.quantity,value.unitPrice,'new' );
-    // this.itemService.storeNewItem(item).subscribe(
-    //   (response)=> console.log(response),
-    //   (error) => console.log(error)
-    // )  
     const value = form.value;
-    const newItem = new Item(value.id,value.itemName,value.brandName,value.description,value.itemImage,value.brandImage,value.quantity,value.unitPrice,value.state);
-    console.log(newItem);
-    this.itemService.itemSelected.emit(newItem);
-    this.itemService.addNewItem(newItem);
-    // console.log(this.itemService.getItem);
+    const item = new Item(1,value.itemName,value.brandName,value.description,this.selectedItemImage,value.brandImage,value.quantity,value.unitPrice,'new' );
+    this.itemService.storeNewItem(item,this.selectedItemImage).subscribe(
+      (response)=> console.log(response),
+      (error) => console.log(error)
+    )  
+    // const value = form.value;
+    // const newItem = new Item(value.id,value.itemName,value.brandName,value.description,value.itemImage,value.brandImage,value.quantity,value.unitPrice,value.state);
+    // console.log(newItem);
+    // this.itemService.itemSelected.emit(newItem);
+    // this.itemService.addNewItem(newItem);
+    // // console.log(this.itemService.getItem);
   }
 
-  toItems(){
-    // this.route.navigate([/retailer/newItems]);
-  }
 
- 
 
  
 
