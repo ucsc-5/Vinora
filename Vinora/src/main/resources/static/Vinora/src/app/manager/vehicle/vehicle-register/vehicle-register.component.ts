e26b29b2db731b777b470b491e02add7f10be0eb
Hashini <hashini.rangala.dias@gmail.com>
2019-10-05 15:08:57 +0530

    image upload not working

--- Vinora/src/main/resources/static/Vinora/src/app/manager/vehicle/vehicle-register/vehicle-register.component.ts
+++ Vinora/src/main/resources/static/Vinora/src/app/manager/vehicle/vehicle-register/vehicle-register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { VehicleService } from '../../../service/vehicle.service';
 import { NgForm } from '@angular/forms';
 import { Vehicle } from '../vehicle.model';
+import { Upload } from 'src/app/uploads/shared/upload';
 
 @Component({
   selector: 'app-vehicle-register',
@@ -9,6 +10,10 @@ import { Vehicle } from '../vehicle.model';
   styleUrls: ['./vehicle-register.component.css']
 })
 export class VehicleRegisterComponent implements OnInit {
+
+  selectedFiles: FileList;
+  currentUpload: Upload;
+
 
   constructor(private vehicleService:VehicleService) { }
 
@@ -19,4 +24,13 @@ export class VehicleRegisterComponent implements OnInit {
     const vehicle = new Vehicle(value.number_plate,value.vehicle_model,value.owner_name,value.owner_mobile,value.owner_address);
     this.vehicleService.createVehicle(vehicle);
   }
+
+  detectFiles(event) {
+    this.selectedFiles = event.target.files;
+}
+uploadSingle() {
+  let file = this.selectedFiles.item(0)
+  this.currentUpload = new Upload(file);
+  this.vehicleService.pushUpload(this.currentUpload)
+}
 }
--- Vinora/src/main/resources/static/Vinora/src/app/service/vehicle.service.ts
+++ Vinora/src/main/resources/static/Vinora/src/app/service/vehicle.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import {FormControl,FormGroup} from '@angular/forms';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { Vehicle } from '../manager/vehicle/vehicle.model';
+import { Upload } from '../uploads/shared/upload';
+import * as firebase from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +11,8 @@ import { Vehicle } from '../manager/vehicle/vehicle.model';
 export class VehicleService {
 
   private dbPath = '/vehicles';
+  private basePath:string = '/uploads';
+  uploads: AngularFireList<Upload[]>;
 
   vehicleRef: AngularFireList<Vehicle> = null;
 
@@ -19,6 +23,35 @@ export class VehicleService {
 
   createVehicle(vehicle: Vehicle): void {
     this.vehicleRef.push(vehicle);
+  }
+
+  pushUpload(upload: Upload) {
+    let storageRef = firebase.storage().ref();
+    let uploadTask = storageRef.child(`${this.basePath}/${upload.file.name}`).put(upload.file);
+
+    uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+      (snapshot) =>  {
+        // upload in progress
+        upload.progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+      },
+      (error) => {
+        // upload failed
+        console.log(error)
+      },
+      () => {
+        // upload success
+        upload.url = uploadTask.snapshot.downloadURL
+        upload.name = upload.file.name
+        this.saveFileData(upload)
+      }
+    );
+  }
+
+
+
+  // Writes the file details to the realtime db
+  private saveFileData(upload: Upload) {
+    this.db.list(`${this.basePath}/`).push(upload);
   }
 
   
