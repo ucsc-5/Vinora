76aa23da8faa8dd0c986519814b9da895343a467
Chamod Lakmal <chamodlakmal97@gmail.com>
2019-10-09 23:25:06 +0530

    validation added pending

--- Vinora/src/main/resources/static/Vinora/src/app/home/register-retailer/register-retailer.component.ts
+++ Vinora/src/main/resources/static/Vinora/src/app/home/register-retailer/register-retailer.component.ts
@@ -29,18 +29,23 @@ export class RegisterRetailerComponent implements OnInit {
 
   ngOnInit() {
     this.firstFormGroup = this._formBuilder.group({
-      myControl1: ['shopName', Validators.required],
-      myControl2: ['', Validators.required]  
+      myControl1: ['', Validators.required],
+      myControl2: ['', Validators.required,Validators.minLength(6)]  
     });
     
     this.secondFormGroup = this._formBuilder.group({
-      myControl3: ['', Validators.required],
+      myControl3: ['', Validators.required,Validators.email],
       myControl4: ['', Validators.required],
-      myControl5: ['', Validators.required]
+      myControl5: ['', Validators.required,Validators.minLength(9),Validators.maxLength(9)]
     });
     
   }
-  
+  get fval(){
+    return this.firstFormGroup.controls;
+  }
+  get sval(){
+    return this.secondFormGroup.controls;
+  }
   async register(){
     const userEmail = this.secondFormGroup.value['myControl3'];
     const password = this.firstFormGroup.value['myControl2'];
