dc3c72b2c2b84269e21644d75603af66f75ed3d9
udulaindunil <udulaindunil@gmail.com>
2019-10-13 00:35:46 +0530

    ret can see reg com and items

--- Vinora/src/main/resources/static/Vinora/src/app/retailer/new-order/ret-ord-comp-element/new-order-item-elemet/new-order-item-elemet.component.ts
+++ Vinora/src/main/resources/static/Vinora/src/app/retailer/new-order/ret-ord-comp-element/new-order-item-elemet/new-order-item-elemet.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Item } from 'src/app/service/item.model';
+import { NgForm } from '@angular/forms';
+import { ItemService } from 'src/app/service/item.service';
 
 @Component({
   selector: 'app-new-order-item-elemet',
@@ -10,9 +12,27 @@ export class NewOrderItemElemetComponent implements OnInit {
 
   @Input() item:Item
 
-  constructor() { }
+  orderedItem : Item;
+
+  constructor(private itemServise: ItemService) { }
 
   ngOnInit() {
+  }
+
+  toCart(form: NgForm){
+    const value = form.value;
+    if(this.item.quantity < value.quantity){
+      console.log('not enough items availabale')
+    }else{
+      const newQuantity =  (this.item.quantity)-(value.quantity);
+      console.log(newQuantity);
+      this.itemServise.updateItem(this.item.key,{quantity : newQuantity});
+      this.orderedItem = new Item(this.item.itemName,this.item.brand,this.item.description,value.quantity,this.item.unitPrice,this.item.state);
+      console.log(this.orderedItem);
+      // this.itemCreated.emit(this.orderedItem);
+    }
+
+
   }
 
 }
