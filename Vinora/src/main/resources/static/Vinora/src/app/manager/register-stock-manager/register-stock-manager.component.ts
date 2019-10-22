eaa2beedfe0065d4f7313824b8c43162ac9d7018
chamodlakmal <chamodlakmal97@gmail.com>
2019-10-22 21:17:53 +0530

    stock manager registration completed

--- Vinora/src/main/resources/static/Vinora/src/app/manager/register-stock-manager/register-stock-manager.component.ts
+++ Vinora/src/main/resources/static/Vinora/src/app/manager/register-stock-manager/register-stock-manager.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
+import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+export interface StockManager{
+  fullName:string;
+  address:string;
+  nic:string;
+  email:string;
+  mobile:string;
+  state:string;
+}
 
 @Component({
   selector: 'app-register-stock-manager',
@@ -12,7 +24,14 @@ export class RegisterStockManagerComponent implements OnInit {
   nic=new FormControl('', [Validators.required]);
   email = new FormControl('', [Validators.required, Validators.email]);
   mobile=new FormControl('', [Validators.required,Validators.minLength(10),Validators.maxLength(10)]);
-  constructor() { }
+  private stockManagerCollection: AngularFirestoreCollection<StockManager>;
+  stockManagers: Observable<StockManager[]>;
+  constructor(private afAuth: AngularFireAuth,private readonly afs: AngularFirestore) { 
+    const uid=this.afAuth.auth.currentUser.uid;
+    console.log("before uid"+uid);
+    this.stockManagerCollection = afs.collection<StockManager>(`companies/${uid}/stockManagers`);
+    this.stockManagers= this.stockManagerCollection.valueChanges();
+  }
 
   ngOnInit() {
   }
@@ -22,7 +41,19 @@ export class RegisterStockManagerComponent implements OnInit {
             '';
   }
   register(){
-    console.log(this.fullName.value)
+    const fullName:string=this.fullName.value;
+    const address:string=this.address.value;
+    const nic:string=this.nic.value;
+    const email:string=this.email.value;
+    const mobile:string=this.mobile.value;
+    const state:string="1";
+    const stockManager1:StockManager={fullName,address,nic,email,mobile,state}
+    this.afAuth.auth.createUserWithEmailAndPassword(this.email.value,this.nic.value).then( (data)=>{
+      console.log("UID"+data.user.uid)
+      this.stockManagerCollection.doc(data.user.uid).set(stockManager1);
+    });
+    
+    
   }
 
 }
