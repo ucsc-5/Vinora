150933643f3d3ef7830b960cab19efb956319628
udulaindunil <udulaindunil@gmail.com>
2019-10-22 21:35:58 +0530

    company service error resolved

--- Vinora/src/main/resources/static/Vinora/src/app/service/retailer.service.ts
+++ Vinora/src/main/resources/static/Vinora/src/app/service/retailer.service.ts
@@ -4,8 +4,9 @@ import { Retailer } from './retailer.model';
 import { Observable, BehaviorSubject, Subject } from 'rxjs';
 import { finalize, switchMap, map } from 'rxjs/operators';
 import { AngularFireStorage } from '@angular/fire/storage';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreCollectionGroup } from '@angular/fire/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Company } from './company.service';
 
 
 export interface Reatiler { shopName: string;
@@ -29,44 +30,22 @@ export class RetailerService {
   dbPath = 'retailers'
   // currentRetailerId;
   
+  private retailerCollection: AngularFirestoreCollection<Reatiler>;
   retailer: Observable<Reatiler[]>;
 
-  private retailerCollection: AngularFirestoreCollection<Reatiler>;
-
+  private companyCollection: AngularFirestoreCollection<Company>
+  companies: Observable<Company[]>
 
   constructor(private storage: AngularFireStorage,private readonly afs: AngularFirestore,private afAuth: AngularFireAuth) {
-    
     this.retailerCollection = afs.collection<Reatiler>('retailers');
-    //  this.currentRetailerId = this.afAuth.auth.currentUser.uid;
+    this.companyCollection = afs.collection<Company>('companies');
   }
+
  
+getAllCompanies(){
 
-// createRetailer(retailer2:Retailer,uid:string) {
-//   console.log(uid+" from the service here");
-//   const basePath = this.dbPath
-//   const filePath = `${basePath}/${retailer2.file.name}${new Date()}`;
-//   const storageRef = this.storage.ref(filePath);
-//   const uploadTask = this.storage.upload(filePath,retailer2.file);
+}
 
-//   uploadTask.snapshotChanges().pipe(
-//     finalize(() => {
-//       storageRef.getDownloadURL().subscribe(downloadURL => {
-//         console.log('File available at', downloadURL);
-          
-//           // this.retailerCollection.add(retailer);
-        
-
-//       });
-//     })
-//   ).subscribe();
-
-//          const shopname11: string = retailer2.shopName
-//           const id = this.afs.createId();
-//           // const retailer1: Reatiler10 = {shopname11,shopname11,shopname11,shopname11};
-//           //this.retailerCollection.doc(id).set(retailer1);
-
-//   return uploadTask.percentageChanges();
-// }
 
 getRetailer(uid:string){
   
