0667bbda9a661ceec4847ed8a12ed0d1a048a64a
Hashini <hashini.rangala.dias@gmail.com>
2019-11-05 12:17:01 +0530

    stock manager updated

--- Vinora/src/main/resources/static/Vinora/src/app/stock-manager/stock-manager-profile/stock-manager-profile.component.ts
+++ Vinora/src/main/resources/static/Vinora/src/app/stock-manager/stock-manager-profile/stock-manager-profile.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { StockManagerId, StockManagerService, StockManager } from 'src/app/service/stock-manager.service';
 import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { FileUpload } from 'src/app/uploads/shared/file-upload';
+import { AngularFireStorage } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-stock-manager-profile',
@@ -9,14 +12,31 @@ import { Observable } from 'rxjs';
 })
 export class StockManagerProfileComponent implements OnInit {
 
+  selectedFiles: FileList;
+  currentFileUpload: FileUpload;
+
   stockManager: Observable<StockManagerId[]>;
   stockManagerEmail: string;
 
 
-  constructor(private StockManagerService:StockManagerService) { }
+  constructor(private StockManagerService:StockManagerService,private afAuth:AngularFireAuth,private storage:AngularFireStorage) {
+    this.stockManagerEmail= this.afAuth.auth.currentUser.email;
+   }
 
   ngOnInit() {
     this.stockManager= this.StockManagerService.getStockManagerByEmail(this.stockManagerEmail);
+  }
+
+  selectFile(event){
+    this.selectedFiles = event.target.files;
+  }
+
+
+  onAddImage(){
+    // const file = this.selectedFiles.item(0);
+    // const filePath = ;
+    // const uploadTask = this.storage.upload(filePath,file);
+
   }
 
 }
