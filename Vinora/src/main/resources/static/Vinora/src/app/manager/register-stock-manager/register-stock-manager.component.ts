518247d0619053fe791386986bd10c7afb16b354
udulaindunil <udulaindunil@gmail.com>
2019-11-19 08:19:24 +0530

    confrim dialog button

--- Vinora/src/main/resources/static/Vinora/src/app/manager/register-stock-manager/register-stock-manager.component.ts
+++ Vinora/src/main/resources/static/Vinora/src/app/manager/register-stock-manager/register-stock-manager.component.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFireFunctions } from '@angular/fire/functions';
 import { StockManagerId, StockManager } from 'src/app/service/stock-manager.service';
+import { DialogService } from 'src/app/service/dialog.service';
 
 
 @Component({
@@ -23,7 +24,7 @@ export class RegisterStockManagerComponent implements OnInit {
   type = 'stockManager';
   companyId:string;
 
-  constructor(private afAuth: AngularFireAuth,private readonly afs: AngularFirestore,private fns: AngularFireFunctions) { 
+  constructor(private dialogService:DialogService,private afAuth: AngularFireAuth,private readonly afs: AngularFirestore,private fns: AngularFireFunctions) { 
     this.companyId=this.afAuth.auth.currentUser.uid;
     // this.stockManagerCollection = afs.collection<StockManager>(`companies/${companyId}/stockManagers`);  // for the chamods code
     this.stockManagerCollection = afs.collection<StockManager>('stockManagers');
@@ -66,29 +67,35 @@ export class RegisterStockManagerComponent implements OnInit {
 
     async register(){
 
-    const companyId=this.afAuth.auth.currentUser.uid;
-    const fullName:string=this.fullName.value;
-    const address:string=this.address.value;
-    const nic:string=this.nic.value;
-    const email:string=this.email.value;
-    const contactNumber:string=this.contactNumber.value;
-    const state:string="1";
-    const imagePath:string="https://www.pureingenuity.com/wp-content/uploads/2018/07/empty-profile-image.jpg";
-    const stockManager1:StockManager={fullName,address,nic,email,contactNumber,state,companyId,imagePath}
-    const callable = await this.fns.httpsCallable('addRole');
-    var createUser=this.afAuth.auth.createUserWithEmailAndPassword(this.email.value,this.nic.value);
-    callable({email:email,role:this.type,companyId:this.companyId}).subscribe(
-      (response)=>{
-           console.log(response);
-      },
-      ()=>{},
-      ()=>{
-        createUser.then( (data)=>{
-          this.stockManagerCollection.doc(data.user.uid).set(stockManager1);
-        });
-   }
-  ) 
-    
+      const message =" Confirm regsitration!"
+      this.dialogService.openConfirmDialog(message).afterClosed().subscribe(
+        res=>{
+          if(res){
+
+            const companyId=this.afAuth.auth.currentUser.uid;
+            const fullName:string=this.fullName.value;
+            const address:string=this.address.value;
+            const nic:string=this.nic.value;
+            const email:string=this.email.value;
+            const contactNumber:string=this.contactNumber.value;
+            const state:string="1";
+            const imagePath:string="https://www.pureingenuity.com/wp-content/uploads/2018/07/empty-profile-image.jpg";
+            const stockManager1:StockManager={fullName,address,nic,email,contactNumber,state,companyId,imagePath}
+            const callable = this.fns.httpsCallable('addRole');
+            var createUser=this.afAuth.auth.createUserWithEmailAndPassword(this.email.value,this.nic.value);
+            callable({email:email,role:this.type,companyId:this.companyId}).subscribe(
+              (response)=>{
+                   console.log(response);
+              },
+              ()=>{},
+              ()=>{
+                createUser.then( (data)=>{
+                  this.stockManagerCollection.doc(data.user.uid).set(stockManager1);
+                });
+           }
+          ) 
+          }
+        })   
   }
 
 }
--- Vinora/src/main/resources/static/Vinora/src/app/manager/sales-representative/register-sales-representative/register-sales-representative.component.ts
+++ Vinora/src/main/resources/static/Vinora/src/app/manager/sales-representative/register-sales-representative/register-sales-representative.component.ts
@@ -7,6 +7,7 @@ import { AngularFireFunctions } from '@angular/fire/functions';
 import { finalize } from 'rxjs/operators';
 import { AngularFireStorage } from '@angular/fire/storage';
 import { SalesRepresentative, SalesRepresentativeId } from 'src/app/service/sales-representative.service';
+import { DialogService } from 'src/app/service/dialog.service';
 
 
 
@@ -26,7 +27,7 @@ export class RegisterSalesRepresentativeComponent implements OnInit {
   companyId:string;
 
 
-  constructor(private afAuth: AngularFireAuth,private readonly afs: AngularFirestore,private fns: AngularFireFunctions,private storage: AngularFireStorage) {
+  constructor(private dialogService:DialogService,private afAuth: AngularFireAuth,private readonly afs: AngularFirestore,private fns: AngularFireFunctions,private storage: AngularFireStorage) {
     this.companyId= this.afAuth.auth.currentUser.uid;
 
     this.salesRepresentativeCollection = afs.collection<SalesRepresentative>(`salesRepresentatives`);
@@ -40,40 +41,46 @@ export class RegisterSalesRepresentativeComponent implements OnInit {
 
 
   async onAddSalesrepresentative(form: NgForm){
-    const value = form.value;
-    const fullName = value.fullName;
-    const address = value.address;
-    const email = value.email;
-    const contactNumber = value.contactNumber;
-    const nic = value.nic;
-    const companyId = this.companyId;
-    const salesRefImagePath:string="https://www.pureingenuity.com/wp-content/uploads/2018/07/empty-profile-image.jpg";
-    const state = "0";
-   
-    const id = this.afs.createId();
 
-    const salesrepresentative:SalesRepresentative = {fullName,address,email,contactNumber,nic,salesRefImagePath,state,companyId};
-    
-    console.log(salesrepresentative);
-    
-    
-    const callable = await this.fns.httpsCallable('addRole');
-    var createUser=this.afAuth.auth.createUserWithEmailAndPassword(email,nic);
-    callable({email:email,role:this.type,companyId:this.companyId}).subscribe(
-      (response)=>{
-           console.log(response);
-      },
-      ()=>{},
-      ()=>{
-        createUser.then( (data)=>{
-          this.salesRepresentativeCollection.doc(id).set(salesrepresentative);
-        });
-   }
-  ) 
+    const message ="Confirm regsitration!"
+    this.dialogService.openConfirmDialog(message).afterClosed().subscribe(
+      res=>{
+        if(res){
+
+          const value = form.value;
+          const fullName = value.fullName;
+          const address = value.address;
+          const email = value.email;
+          const contactNumber = value.contactNumber;
+          const nic = value.nic;
+          const companyId = this.companyId;
+          const salesRefImagePath:string="https://www.pureingenuity.com/wp-content/uploads/2018/07/empty-profile-image.jpg";
+          const state = "0";
+         
+          const id = this.afs.createId();
+      
+          const salesrepresentative:SalesRepresentative = {fullName,address,email,contactNumber,nic,salesRefImagePath,state,companyId};
+          
+          console.log(salesrepresentative);
+          
+          
+          const callable = this.fns.httpsCallable('addRole');
+          var createUser=this.afAuth.auth.createUserWithEmailAndPassword(email,nic);
+          callable({email:email,role:this.type,companyId:this.companyId}).subscribe(
+            (response)=>{
+                 console.log(response);
+            },
+            ()=>{},
+            ()=>{
+              createUser.then( (data)=>{
+                this.salesRepresentativeCollection.doc(id).set(salesrepresentative);
+              });
+         }
+        ) 
 
 
-   
-             
+        }})
+            
 
   }
 
