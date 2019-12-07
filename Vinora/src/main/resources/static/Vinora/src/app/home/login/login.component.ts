3af76f4d62c7eac0c7cd73bdc758217e615fd3b8
Hashini <hashini.rangala.dias@gmail.com>
2019-12-07 09:33:38 +0530

    welcome page content updated

--- Vinora/src/main/resources/static/Vinora/src/app/home/login/login.component.ts
+++ Vinora/src/main/resources/static/Vinora/src/app/home/login/login.component.ts
@@ -63,7 +63,7 @@ export class LoginComponent implements OnInit {
     this.showSpinner=true;
 
     // this.authService.login('admin@gmail.com','123123'); //for admin
-    //  this.authService.login('chamod999@gmail.com','0714954149'); //for stock manager
+     this.authService.login('chamod999@gmail.com','0714954149'); //for stock manager
     // this.authService.login('ret1@gmail.com','123123'); //for retailer
     // this.authService.login('vigith@gmail.com','123123'); //for retailer
     // this.authService.login('vigith@gmail.com','123123'); //for retailer
