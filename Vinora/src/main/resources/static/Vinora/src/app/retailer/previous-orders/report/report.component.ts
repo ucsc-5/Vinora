22dbf3b6700e925cbbd0e4d235e001bcf0e0f9b6
Hashini <hashini.rangala.dias@gmail.com>
2020-01-10 18:31:56 +0530

    report service added and updated

--- Vinora/src/main/resources/static/Vinora/src/app/retailer/previous-orders/report/report.component.ts
+++ Vinora/src/main/resources/static/Vinora/src/app/retailer/previous-orders/report/report.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CompanyId, CompanyService } from 'src/app/service/company.service';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { ReportService } from 'src/app/service/report.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { OrderId, OrderService } from 'src/app/service/order.service';
 
 @Component({
   selector: 'app-report',
@@ -14,8 +17,13 @@ export class ReportComponent implements OnInit {
   companyId: string;
 
   company: Observable<CompanyId[]>
+  confirmedOrders:Observable<OrderId[]>;
+  @Input() confirmOrder: OrderId;
+  retailerId;
  
-  constructor(private router:Router,private companyService: CompanyService, private route:ActivatedRoute) { }
+  constructor(private router:Router,private companyService: CompanyService, private route:ActivatedRoute,private reportService:ReportService, private afAuth: AngularFireAuth,private orderService: OrderService) { 
+    this.retailerId= this.afAuth.auth.currentUser.uid;
+  }
 
   ngOnInit() {
     this.route.params.subscribe((param:Params)=>{
@@ -24,6 +32,24 @@ export class ReportComponent implements OnInit {
    
    
     this.company= this.companyService.getCompanyById(this.companyId);
+
+    // this.reportService.getreportdetails(this.companyId).subscribe{
+
+    // }
+
+    this.confirmedOrders = this.orderService.getConfirmedOrdersByRetailerId(this.retailerId);
+    this.confirmedOrders.subscribe(x=>{
+      console.log(x);
+      
+    });
+
+    this.company = this.companyService.getCompanyById(this.confirmOrder.companyId);
+    this.company.subscribe(y=>{
+      console.log("hashini"+y);
+      
+    });
+
+    console.log(this.confirmOrder.createDate);
   }
 
 }
---
+++ Vinora/src/main/resources/static/Vinora/src/app/service/report.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { OrderId } from './order.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReportService {
+
+  
+
+
+  constructor() { 
+
+  }
+
+  getreportdetails(companyId:string){
+
+  }
+}
