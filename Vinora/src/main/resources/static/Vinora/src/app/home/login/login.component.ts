3d5c31ea1c9642cc80bde48d2026a4b2af55da00
Chamindu Udesh <udeshchamindu@gmail.com>
2020-01-22 00:23:22 +0530

    edit login page

--- Vinora/src/main/resources/static/Vinora/src/app/home/login/login.component.ts
+++ Vinora/src/main/resources/static/Vinora/src/app/home/login/login.component.ts
@@ -69,10 +69,10 @@ export class LoginComponent implements OnInit {
     // this.authService.login('vigith@gmail.com','123123'); //for retailer
     // this.authService.login('royalvintage@gmail.com','654321'); //for Manager
    
-    this.authService.login('udulaindunil@gmail.com','#Udula@1997'); //for stock manager Manager email verified
+    // this.authService.login('udulaindunil@gmail.com','#Udula@1997'); //for stock manager Manager email verified
     // this.authService.login('2017cs162@stu.ucsc.cmb.ac.lk','#Udula@1997'); //for stock manager Manager email verified
 
-        // this.authService.login('udulastm@gmail.com','0123456789v'); //for Manager
+        this.authService.login('2017cs162@stu.ucsc.cmb.ac.lk','#Udula@1997'); //for Manager
         //  this.authService.login('udulacompany@gmail.com','#Vinora@123'); //for Manager
     this.showSpinner=false;
     console.log(this.authService.user.uid); 
